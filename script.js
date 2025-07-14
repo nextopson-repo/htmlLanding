108dd34cfe1cba0d409225c0635a98bf93e6fd91
John Doe <tanikshaverma966@gmail.com>
2025-07-14 19:53:27 +0530

    message

--- script.js
+++ script.js
@@ -86,61 +86,10 @@ function alertCTA() {
   alert("Thank you for showing interest! We'll get back to you soon.");
 }
 
-function toggleFaq(el) {
-  const icon = el.querySelector('i');
-  const isOpen = el.classList.toggle('open');
-  const answer = el.querySelector('.faq-answer');
-  icon.classList.toggle('fa-chevron-down', !isOpen);
-  icon.classList.toggle('fa-chevron-up', isOpen);
-  if (isOpen) {
-    answer.style.display = 'block';
-  } else {
-    answer.style.display = 'none';
-  }
-}
+// 
 
 
-  // Categery icons
- 
-    // Slides for Row 1
-    const slides1 = [
-      { img: "./Images/building-02-solid-rounded.svg", text: "Builder Floors" },
-      { img: "./Images/house-03-solid-rounded.svg", text: "House/Villa" },
-      { img: "./Images/house-04-solid-rounded.svg", text: "Farmhouse" },
-      { img: "./Images/qq-plot-solid-rounded.svg", text: "Plots" }
-    ];
 
-    // Slides for Row 2
-    const slides2 = [
-     
-      { img: "./Images/store-03-solid-rounded.svg", text: "Shops/Showroom" },
-      { img: "./Images/office-solid-rounded.svg", text: "Office Spaces" },
-      { img: "./Images/hotel-01-solid-rounded.svg", text: "Hotels" },
-      { img: "./Images/hotel-02-solid-rounded.svg", text: "Hostel/PG" }
-    ];
-
-    // Set up each carousel
-    function setupCarousel(slides, containerId, delay = 2000) {
-      let current = 0;
-      const carousel = document.getElementById(containerId);
-
-      function showSlide(index) {
-        const slide = slides[index];
-        carousel.innerHTML = `
-          <img src="${slide.img}" class="w-16 h-16 object-contain" />
-          <span class="text-md font-medium mt-2">${slide.text}</span>
-        `;
-      }
-
-      // Start interval
-      setInterval(() => {
-        current = (current + 1) % slides.length;
-        showSlide(current);
-      }, delay);
-    }
-
-    // Start both carousels
-    setupCarousel(slides1, "carousel1");
-    setupCarousel(slides2, "carousel2");
-
+   
+  
       
